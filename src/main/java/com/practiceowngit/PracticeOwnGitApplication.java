f0cba0406e127b13b07408302aedba3b75468c1c
pavaniBv <venkatapavani.b@thrymr.net>
2023-12-08 12:36:48 +0530

    first commit

---
+++ src/main/java/com/practiceowngit/PracticeOwnGitApplication.java
@@ -0,0 +1,13 @@
+package com.practiceowngit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeOwnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeOwnGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practiceowngit/PracticeOwnGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceowngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeOwnGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
